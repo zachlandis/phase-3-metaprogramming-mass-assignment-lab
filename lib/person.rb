8968872e40cae3ae034f7d003c8eefdd202f7e3d
Zach <zachlandis91@gmail.com>
2023-04-18 12:43:35 -0600

    commit tree 58e1bc38a8be81736af2d6c7b80ee896cfb67d1b

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,13 @@
 class Person
-  # your code here
+
+  attributes = :name, :birthday, :hair_color, :eye_color, :height,
+  :weight, :handed, :complexion, :t_shirt_size,
+  :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
